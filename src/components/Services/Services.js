eb29706a4245bbe871b80f508854c452b504a60c
ayat-liberatelabs <ayat@liberate-labs.com>
2023-11-18 20:42:25 +0600

    Spelling changed for apartment

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -17,7 +17,7 @@ const services = [
     },
     {
         title: "Property",
-        list: ['Villas for rent', 'Apartment for rent', 'Hotels'],
+        list: ['Villas for rent', 'Apartments for rent', 'Hotels'],
         img: require('@/assets/images/villa.jpeg').default.src,
         to: 'property#top'
     },
